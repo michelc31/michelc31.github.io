27a0fd63426500a4907ae80d48ec3f73139443cb
Omar Michel Carmona Villalobos <135045145+michelc31@users.noreply.github.com>
2024-03-08 20:07:50 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const https = require('https');
+
+const client_id = 'a4ef090bc66b496189efaf77a9cd0a18';
+const client_secret = '7c623cda354f465888502a111aa7fb55';
+const track_id = 'Ella';
+
+async function getAccessToken() {
+    const endpoint = 'https://accounts.spotify.com/api/token';
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: 'grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}'
+    };
+    const response = await fetch(endpoint_spotify, requestOptions);
+    return response.json().then(data => data.access_token);
+}
+
+async function getTrackInfo(track_id, access_token) {
+    const endpoint_search_spotify = `https://api.spotify.com/v1/search?q=${track_id}&type=track&limit=1`;
+    const requestOptions = {
+        headers: {
+            'Authorization': `Bearer ${access_token}`
+        }
+    };
+
+    const response = await fetch(endpoint_search_spotify, requestOptions);
+    return response.json();
+}
+
+const server = https.createServer(
+    async function(req, res){
+        const access_token = await getAccessToken();
+        const trackInfo = await getTrackInfo(track_id, access_token);
+        res.write(JSON.stringify(trackInfo, null, 2));
+        console.log(trackInfo);
+        res.end();
+    }
+);
+server.listen(3000, '127.0.0.1',function(){
+    console.log('servidor escuchando')
+
+});
---
+++ script.js
@@ -0,0 +1,41 @@
+function obtenerRespuesta() {
+    const termino = document.getElementById('inputTexto').value;
+    const endpointOpenAI = "https://api.openai.com/v1/chat/completions";
+    const opcionesOpenAI = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer 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'
+        },
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: [{
+                "role": "user",
+                "content": "Obtener respuesta"
+            }, {
+                "role": "system",
+                "content": termino // Aquí proporcionamos el término ingresado como contenido
+            }]
+        })
+    };
+
+    fetch(endpointOpenAI, opcionesOpenAI)
+        .then(function(respuesta) {
+            return respuesta.json();
+        })
+        .then(function(response) {
+            const respuestaAI = response.choices[0].message.content;
+            mostrarResultado(respuestaAI);
+        })
+        .catch(function(error) {
+            console.error('Hubo un problema con la solicitud a OpenAI:', error);
+        });
+}
+
+function mostrarResultado(respuestaAI) {
+    const resultadoDiv = document.getElementById('resultado');
+    resultadoDiv.innerHTML = `
+        <h4>Respuesta de ChatGPT:</h4>
+        <p>${respuestaAI}</p>
+    `;
+}
